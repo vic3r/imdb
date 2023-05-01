6a6b69b0d8e9759b4c87aa5c06ee045683ba0cc2
vic3r <victor.g64r@gmail.com>
2023-04-30 21:50:46 -0600

    Create the header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>HOME</h1>;
+  return <h1 className="text-red-400">HOME</h1>;
 }
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import React from 'react';
+import { IconType } from 'react-icons';
+
+type MenuItemProps = {
+  title: string;
+  address: string;
+  Icon: IconType;
+};
+
+function MenuItem({ title, address, Icon }: MenuItemProps) {
+  return (
+    <div>
+      <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600">
+        <Icon className="text-2xl sm:hidden mx-4" />
+        <p className="hidden sm:inline my-2 text-sm">{title}</p>
+      </Link>
+    </div>
+  );
+}
+
+export default MenuItem;
