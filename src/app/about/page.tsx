746d49fa7466f4f80abeadbabff94c55f65848e3
vic3r <victor.g64r@gmail.com>
2023-05-11 15:54:31 -0600

    Add Navbar component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function About() {
   return (
     <div className="max-w-6xl mx-auto space-y-4 p-4">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Providers from './Providers';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <Header />
+          <Navbar />
           {children}
         </Providers>
       </body>
