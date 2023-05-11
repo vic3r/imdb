bbe082021c4580d684c488c888c1fcb4ccebbd26
vic3r <victor.g64r@gmail.com>
2023-05-11 15:47:42 -0600

    Create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-x-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <h1>
+        <p>Welcome to our movie database website</p>
+        <p>
+          Our website is deigned to provide specific reviews about movies and tv
+          shows
+        </p>
+        <p>
+          In addition there is nothing to worry about the movies and tv shows
+          that have been unreleased
+        </p>
+      </h1>
+    </div>
+  );
+}
+
+export default About;
