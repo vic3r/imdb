cd2708c533384fe081b399362da14e33d407b5a4
vic3r <victor.g64r@gmail.com>
2023-05-14 18:48:29 -0600

    Create Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import NavbarItem from './NavbarItem';
 
 function Navbar() {
   return (
-    <div>
+    <div className="flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4">
       <NavbarItem title="Trending" param="fetchTrending" />
       <NavbarItem title="Top Rated" param="fetchTopRated" />
     </div>
--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -1,12 +1,29 @@
+'use client';
+
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+
 type NavbarItemProps = {
   title: string;
   param: string;
 };
 
 function NavbarItem({ title, param }: NavbarItemProps) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
+
   return (
     <div>
-      <h1>{title}</h1>
+      <Link
+        className={`m-4 hover:text-amber-600 font-semibold p-2 ${
+          genre &&
+          genre === param &&
+          'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
     </div>
   );
 }
